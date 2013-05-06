e93c98dd7fa9686a0d36a1d8bb32ebd0cabb57e1
arti4077 <arti4077@8c7c4fba-48f7-889f-d39c-888b0d1bbe2e>
2013-05-06 12:59:50 +0000

    po polaczeniu spolek

---
+++ eodt/trunk_v2_02/trunk_03/trunk_03/src/java/pl/eod/managed/StruktOsM.java
@@ -0,0 +1,83 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.eod.managed;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+import pl.eod.encje.DzialJpaController;
+import pl.eod.encje.Struktura;
+import pl.eod.encje.StrukturaJpaController;
+import pl.eod.encje.Uzytkownik;
+import pl.eod.encje.UzytkownikJpaController;
+
+/**
+ *
+ * @author 103039
+ */
+@ManagedBean(name = "StruktOsM")
+@SessionScoped
+public class StruktOsM implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    UzytkownikJpaController userC;
+    StrukturaJpaController struktC;
+    DzialJpaController dzialC;
+    private List<Struktura> srcRoots = new ArrayList<Struktura>();
+    @ManagedProperty(value = "#{login}")
+    private Login login;
+
+    @PostConstruct
+    public void init() {
+        userC = new UzytkownikJpaController();
+        struktC = new StrukturaJpaController();
+        dzialC = new DzialJpaController();
+    }
+
+    public String lista() {
+        return "/all/strukturaOs";
+    }
+
+    public synchronized List<Struktura> getSourceRoots() {
+        Struktura firma = new Struktura();
+        Uzytkownik uFirma = new Uzytkownik();
+        List<Struktura> wynik = new ArrayList<Struktura>();
+        uFirma.setFullname("Organizacja - wg pracowników");
+        firma.setUserId(uFirma);
+        srcRoots.clear();
+        if (login.isAdmin()) {
+            srcRoots.addAll(struktC.getFindBezSzefa());
+            firma.setBezpPod(srcRoots);
+            wynik.add(firma);
+        } else {
+            for (Struktura s : struktC.findGeneryczny().getBezpPod()) {
+                if (s.getUserId().getSpolkaId().equals(login.zalogowany.getUserId().getSpolkaId())) {
+                    wynik.add(s);
+                }
+            }
+        }
+        return wynik;
+    }
+
+    public DzialJpaController getDzialC() {
+        return dzialC;
+    }
+
+    public StrukturaJpaController getStruktC() {
+        return struktC;
+    }
+
+    public Login getLogin() {
+        return login;
+    }
+
+    public void setLogin(Login login) {
+        this.login = login;
+    }
+}
---
+++ eodt/trunk_v2_02/trunk_03/trunk_03/src/java/pl/eod2/managedRej/Rejestracja.java
@@ -0,0 +1,192 @@
+package pl.eod2.managedRej;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
+import pl.eod.managed.Login;
+import pl.eod2.encje.DcDokument;
+import pl.eod2.encje.DcDokumentJpaController;
+import pl.eod2.encje.DcDokumentStatus;
+import pl.eod2.encje.DcDokumentStatusJpaController;
+import pl.eod2.encje.DcPlik;
+import pl.eod2.encje.DcPlikJpaController;
+import pl.eod2.encje.exceptions.IllegalOrphanException;
+import pl.eod2.encje.exceptions.NonexistentEntityException;
+
+@ManagedBean(name = "RejestracjaRej")
+@SessionScoped
+public class Rejestracja {
+
+    private DataModel<DcDokument> lista = new ListDataModel<DcDokument>();
+    private DcDokumentJpaController dcC;
+    private DcPlikJpaController dcPlikC;
+    private DcDokumentStatusJpaController dcDokStatC;
+    private DcDokument obiekt;
+    private DcPlik plik;
+    private String error;
+    @ManagedProperty(value = "#{login}")
+    private Login login;
+
+    @PostConstruct
+    void init() {
+        dcC = new DcDokumentJpaController();
+        dcPlikC = new DcPlikJpaController();
+        dcDokStatC=new DcDokumentStatusJpaController();
+        refresh(true);
+    }
+
+    void refresh(boolean obiektTak) {
+        lista.setWrappedData(dcC.findDcDokumentEntities());
+        if(obiektTak) obiekt = new DcDokument();
+        error = null;
+    }
+
+    public void dodaj() {
+        obiekt.setUserId(login.getZalogowany().getUserId());
+        DcDokumentStatus dokSt= dcDokStatC.findDcDokumentStatus(1);
+        obiekt.setDokStatusId(dokSt);
+        error = dcC.create(obiekt);
+        if (error != null) {
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, error, error);
+            FacesContext context = FacesContext.getCurrentInstance();
+            UIComponent zapisz = UIComponent.getCurrentComponent(context);
+            context.addMessage(zapisz.getClientId(context), message);
+        } else {
+            refresh(true);
+        }
+    }
+    
+    public void wyslijDoAkceptacji(){
+        obiekt=dcC.wyslijDoAkceptacji(obiekt);
+        refresh(false);
+    }
+
+    public void edytuj() {
+        try {
+            error=dcC.edit(obiekt);
+        } catch (IllegalOrphanException ex) {
+            Logger.getLogger(Rejestracja.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NonexistentEntityException ex) {
+            Logger.getLogger(Rejestracja.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(Rejestracja.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        //System.err.println(error);
+        if (error != null) {
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, error, error);
+            FacesContext context = FacesContext.getCurrentInstance();
+            UIComponent zapisz = UIComponent.getCurrentComponent(context);
+            context.addMessage(zapisz.getClientId(context), message);
+            lista.setWrappedData(dcC.findDcDokumentEntities());
+        } else {
+            refresh(true);
+        }
+    }
+    
+    public void edytujZdetale() {
+        try {
+            error=dcC.edit(obiekt);
+        } catch (IllegalOrphanException ex) {
+            Logger.getLogger(Rejestracja.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NonexistentEntityException ex) {
+            Logger.getLogger(Rejestracja.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(Rejestracja.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        //System.err.println(error);
+        if (error != null) {
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, error, error);
+            FacesContext context = FacesContext.getCurrentInstance();
+            UIComponent zapisz = UIComponent.getCurrentComponent(context);
+            context.addMessage(zapisz.getClientId(context), message);
+            lista.setWrappedData(dcC.findDcDokumentEntities());
+        } else {
+            refresh(false);
+        }
+    }
+
+    public void usun() throws IllegalOrphanException, NonexistentEntityException {
+        dcC.destroy(obiekt.getId());
+        refresh(true);
+    }
+    
+    public void usunPlik() throws IllegalOrphanException, NonexistentEntityException {
+        dcPlikC.destroy(plik.getId());
+        obiekt.getDcPlikList().remove(plik);
+        edytujZdetale();
+    }
+
+    public String list() {
+        refresh(true);
+        return "/dcrej/dokumentList";
+    }
+    
+    public String detale() {
+        return "/dcrej/dokumentDetale?faces-redirect=true";
+    }
+
+    public DataModel<DcDokument> getLista() {
+        return lista;
+    }
+
+    public void setLista(DataModel<DcDokument> lista) {
+        this.lista = lista;
+    }
+
+    public DcDokument getObiekt() {
+        return obiekt;
+    }
+
+    public void setObiekt(DcDokument obiekt) {
+        this.obiekt = obiekt;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public Login getLogin() {
+        return login;
+    }
+
+    public void setLogin(Login login) {
+        this.login = login;
+    }    
+
+    public DcDokumentJpaController getDcC() {
+        return dcC;
+    }
+
+    public void setDcC(DcDokumentJpaController dcC) {
+        this.dcC = dcC;
+    }
+
+    public DcPlikJpaController getDcPlikC() {
+        return dcPlikC;
+    }
+
+    public void setDcPlikC(DcPlikJpaController dcPlikC) {
+        this.dcPlikC = dcPlikC;
+    }
+
+    public DcPlik getPlik() {
+        return plik;
+    }
+
+    public void setPlik(DcPlik plik) {
+        this.plik = plik;
+    }
+    
+}
