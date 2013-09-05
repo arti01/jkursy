1fee2f9074a9c1e17d8d0a373edc47209ecf5658
arti4077 <arti4077@8c7c4fba-48f7-889f-d39c-888b0d1bbe2e>
2013-09-05 15:56:09 +0000

    testy obrazka z bazy

--- eodt/trunk_v2_02/trunk_03/src/java/pl/eod/wydruki/DcDokPoczta.java
+++ eodt/trunk_v2_02/trunk_03/src/java/pl/eod/wydruki/DcDokPoczta.java
@@ -4,6 +4,7 @@
  */
 package pl.eod.wydruki;
 
+import org.apache.commons.codec.binary.Base64;
 import pl.eod2.encje.DcDokument;
 
 /**
@@ -15,12 +16,16 @@ public class DcDokPoczta {
     String nazwa;
     String kontrahentNazwa;
     String kontrahentAdres;
+    String obraz=null;
 
     DcDokPoczta(DcDokument doc){
         id=doc.getId();
         nazwa=doc.getNazwa();
         kontrahentNazwa=doc.getKontrahentId().getNazwa();
         kontrahentAdres =doc.getKontrahentId().getAdres();
+        /*if(doc.getDcPlikList()!=null&&!doc.getDcPlikList().isEmpty()){
+            obraz=""+ Base64.encodeBase64String(doc.getDcPlikList().get(0).getPlik());
+        }*/
     }
     
     DcDokPoczta(){
@@ -56,6 +61,14 @@ public class DcDokPoczta {
 
     public void setKontrahentAdres(String kontrahentAdres) {
         this.kontrahentAdres = kontrahentAdres;
+    }
+
+    public String getObraz() {
+        return obraz;
+    }
+
+    public void setObraz(String obraz) {
+        this.obraz = obraz;
     }
     
 }
